1dbc85301aa53af0ff820266883ec4a594016c1e
harshans22 <harshhub.414@gmail.com>
2024-10-08 11:16:42 +0530

    authentication started

---
+++ src/main/java/com/example/auth/auth/Entity/Employee.java
@@ -0,0 +1,31 @@
+package com.example.auth.auth.Entity;
+
+import org.springframework.data.annotation.Id;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Builder
+public class Employee {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String name;
+    private String email;
+    private String password;
+    private String role;
+    private String address;
+    private String phone;
+
+}
